463d23585918b7e90c3ce7cd24feb12915fd761d
park cu <thsbmisf@naver.com>
2022-05-03 12:44:30 +0900

    랜덤 연습

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
+	int R = 0;
+	int F = 0;
+	int A[10] = { 0, };
 
+	srand(static_cast<unsigned int>(time(0)));
 
+	for (int j = 10; j >= 1; --j)
+	{
+		for (int i = 1; i <= j; ++i)
+		{
+			cout << "*";
+
+		}
+		cout << '\\n';
+	}
 
 	return 0;
 }
